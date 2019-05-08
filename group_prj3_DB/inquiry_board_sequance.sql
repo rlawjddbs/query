5b6a76b869a6be2755026f7893614291e1e5a826
rlawjddbs <zeongyun@gmail.com>
2019-05-08 20:50:41 +0900

    20:50

--- group_prj3_DB/inquiry_board_sequance.sql
+++ group_prj3_DB/inquiry_board_sequance.sql
@@ -4,7 +4,7 @@ drop sequence seq_inquiry_num;
 --------------------------DDL for sequence seq_inquiry_num--------------------------------
 create sequence seq_inquiry_num
 start with 1
-maxvalue 9999 /* 4°³ */
+maxvalue 9999999999 /* 4°³ */
 increment by 1
 cycle
 cache 15;
